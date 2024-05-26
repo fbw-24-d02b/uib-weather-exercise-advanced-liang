8a174e7346c19df3733c0b57f51e35ef9e1e3f6f
Liang_Wang <liangwang.fr@gmail.com>
2024-05-26 23:52:00 +0200

    update

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ task list:
     --static layout for general weather informaiton-- done
     --fetch weather data from OpenWeatherMap API-- done
     --add an form to refresh location and weather data-- done
+    --change the image for the weather description automaticly-- 
     --add cancel button and esc function to hide the location-input-form-- 
     --switch location by clicking on the location icon-- 
     --switch the background image on changing locations--
